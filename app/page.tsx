9a31da25dd1877fa196e9d7b3e5cc61b299ce3f4
GRACENOBLE <gracenoble72@gmail.com>
2024-06-12 23:02:40 +0300

    stable auth

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import { Button } from "@/components/ui/button";
 const page = () => {
   return (
     <section className="grid place-items-center h-screen">
+      
       {/* <div className="flex gap-4 justify-center items-center h-96 w-96 border rounded-md">
         <Button className="bg-white text-blue-600 border-blue-600 border hover:bg-blue-600 hover:text-white">
           <LoginLink>Login</LoginLink>
