b5054f5f0f200c461f4f95f4e0248acd82677be7
GRACENOBLE <gracenoble72@gmail.com>
2024-06-12 18:32:58 +0300

    Finalized the sign in

--- app/homepage/page.tsx
+++ app/homepage/page.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
-const page = () => {
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import Image from 'next/image';
+
+const page = async() => {
+
+     const {
+       getAccessToken,
+       getBooleanFlag,
+       getFlag,
+       getIdToken,
+       getIntegerFlag,
+       getOrganization,
+       getPermission,
+       getPermissions,
+       getRoles,
+       getStringFlag,
+       getUser,
+       getUserOrganizations,
+       isAuthenticated,
+     } = getKindeServerSession();
+     const user = await getUser()
+     console.log(user);
+     //console.log(await isAuthenticated());
   return (
     <div>
       HomePage
+      <div>{user?.given_name}{user?.family_name}</div>
       <LogoutLink>Log out</LogoutLink>
     </div>
   );
